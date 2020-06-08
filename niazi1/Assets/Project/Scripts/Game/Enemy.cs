33cb78829d6afb982d0148b69460cd8507513df5
iamniazi <65233487+iamniazi@users.noreply.github.com>
2020-06-08 14:49:26 +0500

    add project

---
+++ niazi1/Assets/Project/Scripts/Game/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+
+	public int health = 5;
+	public int damage = 5;
+
+	private bool killed = false;
+	public bool Killed { get { return killed; } }
+
+	void OnTriggerEnter(Collider otherCollider)
+	{
+		if (otherCollider.GetComponent<Bullet>() != null)
+		{
+			Bullet bullet = otherCollider.GetComponent<Bullet>();
+			if (bullet.ShotByPlayer == true)
+			{
+				health -= bullet.damage;
+				bullet.gameObject.SetActive(false);
+
+				if (health <= 0)
+				{
+					if (killed == false)
+					{
+						killed = true;
+						OnKill();
+					}
+				}
+			}
+		}
+	}
+
+	protected virtual void OnKill()
+	{
+
+		//SceneManager.LoadScene("Menu");
+	}
+}
---
+++ niazi1/Assets/Project/Scripts/Menu/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    void Awake()
+    {
+        // To show cursor after First Person Controller
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void Level_1 () {
+		SceneManager.LoadScene ("level1");
+	}
+
+    public void Level_2()
+    {
+        SceneManager.LoadScene("b");
+    }
+
+    public void Level_3()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
